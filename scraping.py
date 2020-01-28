9ac6dd9979cbeb8e9da55f399f9b7769dbb4658a
drake hilliard <drake.hilliard@opcity.com>
2020-01-27 23:33:53 -0600

    options into for loop, seems more accurate

--- scraping.py
+++ scraping.py
@@ -5,25 +5,27 @@ import time
 
 bets = pd.DataFrame(columns=['site','sport','team_1','team_2','team_1_bet','team_2_bet'])
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--headless')
-
 url_info = {'Site': ['Bovada',
+                     'Bovada',
                      'Bovada'],
             'Sport': ['NBA',
-                      'NFL'],
+                      'NFL',
+                      'NHL/VHL'],
             'URL':['https://www.bovada.lv/sports/basketball/nba',
-                   'https://www.bovada.lv/sports/football'],}
+                   'https://www.bovada.lv/sports/football',
+                   'https://www.bovada.lv/sports/hockey'],}
 
-sites = url_info['Site']
-sports = url_info['Sport']
-urls = url_info['URL']
+sites, sports, urls = url_info['Site'], url_info['Sport'], url_info['URL']
 
 for site, sport, url in zip(sites, sports, urls):
-    # driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=options)
+    # should be able to set below up top
+    # seems to work better when redone each time though
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
     driver = webdriver.Chrome("/Users/drake.hilliard/Desktop/chromedriver", options=options)
+    # should be able to set above up top
     driver.get(url)
     more_buttons = driver.find_elements_by_class_name("moreLink")
     for x in range(len(more_buttons)):
@@ -35,6 +37,10 @@ for site, sport, url in zip(sites, sports, urls):
     soup = BeautifulSoup(page_source)
     team_names = [tag.text for tag in soup.find_all(class_=["name"])]
     bet_prices = [tag.text for tag in soup.find_all(class_=["bet-price","empty-bet"])]
+
+    # CURRENTLY ONLY INTERESTED IN MONEY LINES (3rd and 4th), spreads are 1 and 2, over/under 5 and 6
+    # spreads = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (1,2)]
+    # over_under = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (5,6)]
 
     money_lines = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (3,4)]
     teams_bets = [(i,t,b) for i,t,b in zip(range(len(team_names)),team_names, money_lines)]
@@ -54,4 +60,4 @@ for site, sport, url in zip(sites, sports, urls):
 
     bets = bets.append(bets_df, ignore_index=True)
 
-print(bets)
+bets
