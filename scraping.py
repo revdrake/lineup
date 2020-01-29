7877e2414357d301516c7513dd0e7744eb63a9f5
drake hilliard <drake.hilliard@opcity.com>
2020-01-28 22:00:17 -0600

    add other site

--- scraping.py
+++ scraping.py
@@ -1,3 +1,4 @@
+# BOVADA
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -43,6 +44,86 @@ for site, sport, url in zip(sites, sports, urls):
     # over_under = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (5,6)]
 
     money_lines = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (3,4)]
+    teams_bets = [(i,t,b) for i,t,b in zip(range(len(team_names)),team_names, money_lines)]
+
+    team_1 = [tb[1] for tb in teams_bets if tb[0]%2 == 0]
+    team_2 = [tb[1] for tb in teams_bets if tb[0]%2 != 0]
+    team_1_bet = [tb[2] for tb in teams_bets if tb[0]%2 == 0]
+    team_2_bet = [tb[2] for tb in teams_bets if tb[0]%2 != 0]
+
+
+    bets_df = pd.DataFrame({'site': site,
+                            'sport':sport,
+                            'team_1': team_1,
+                            'team_2': team_2,
+                            'team_1_bet': team_1_bet,
+                            'team_2_bet': team_2_bet,})
+
+    bets = bets.append(bets_df, ignore_index=True)
+
+bets
+
+# BETONLINE AND BOVADA
+# SOMETIMES MISSING SOME
+# AND HOW TO IMPROVE
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+bets = pd.DataFrame(columns=['site','sport','team_1','team_2','team_1_bet','team_2_bet'])
+site_tags = {'Bovada':{'Name': ['name'], 'Lines':['bet-price','empty-bet']},
+              'BetOnline':{'Name':['col_teamname bdevtt'],'Lines':['odds bdevtt moneylineodds displayOdds']}
+             }
+
+url_info = {'Site': ['BetOnline',
+                     'BetOnline',
+                     'Bovada',
+                     'Bovada',
+                     'Bovada'],
+            'Sport': ['NFL',
+                      'NHL/VHL',
+                      'NBA',
+                      'NFL',
+                      'NHL/VHL'],
+            'URL':['https://www.betonline.ag/sportsbook/football/nfl',
+                   'https://www.betonline.ag/sportsbook/hockey/nhl',
+                   'https://www.bovada.lv/sports/basketball/nba',
+                   'https://www.bovada.lv/sports/football',
+                   'https://www.bovada.lv/sports/hockey'],}
+
+sites, sports, urls = url_info['Site'], url_info['Sport'], url_info['URL']
+
+for site, sport, url in zip(sites, sports, urls):
+    # should be able to set below up top
+    # seems to work better when redone each time though
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    driver = webdriver.Chrome("/Users/drake.hilliard/Desktop/chromedriver", options=options)
+    # should be able to set above up top
+    driver.get(url)
+    more_buttons = driver.find_elements_by_class_name("moreLink")
+    for x in range(len(more_buttons)):
+        if more_buttons[x].is_displayed():
+            driver.execute_script("arguments[0].click();", more_buttons[x])
+            time.sleep(5)
+    page_source = driver.page_source
+
+    soup = BeautifulSoup(page_source)
+#     team_names = [tag.text for tag in soup.find_all(class_=["col_teamname bdevtt"])]
+#     money_lines = [tag.text for tag in soup.find_all(class_=["odds bdevtt moneylineodds displayOdds"])]
+
+    team_names = [tag.text for tag in soup.find_all(class_=site_tags[site]['Name'])]
+
+    if site == 'BetOnline':
+        money_lines = [tag.text for tag in soup.find_all(class_=site_tags[site]['Lines'])]
+    elif site == 'Bovada':
+        bet_prices = [tag.text for tag in soup.find_all(class_=site_tags[site]['Lines'])]
+        money_lines = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (3,4)]
+
+
     teams_bets = [(i,t,b) for i,t,b in zip(range(len(team_names)),team_names, money_lines)]
 
     team_1 = [tb[1] for tb in teams_bets if tb[0]%2 == 0]
