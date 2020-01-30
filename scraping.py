cc80e2ffb4c6469319172d2934fc2c39b9770158
drake hilliard <drake.hilliard@opcity.com>
2020-01-30 01:45:13 -0600

    force sleep time, join, set money_line factors

--- scraping.py
+++ scraping.py
@@ -1,75 +1,8 @@
-# BOVADA
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
-
-bets = pd.DataFrame(columns=['site','sport','team_1','team_2','team_1_bet','team_2_bet'])
-
-url_info = {'Site': ['Bovada',
-                     'Bovada',
-                     'Bovada'],
-            'Sport': ['NBA',
-                      'NFL',
-                      'NHL/VHL'],
-            'URL':['https://www.bovada.lv/sports/basketball/nba',
-                   'https://www.bovada.lv/sports/football',
-                   'https://www.bovada.lv/sports/hockey'],}
-
-sites, sports, urls = url_info['Site'], url_info['Sport'], url_info['URL']
-
-for site, sport, url in zip(sites, sports, urls):
-    # should be able to set below up top
-    # seems to work better when redone each time though
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome("/Users/drake.hilliard/Desktop/chromedriver", options=options)
-    # should be able to set above up top
-    driver.get(url)
-    more_buttons = driver.find_elements_by_class_name("moreLink")
-    for x in range(len(more_buttons)):
-        if more_buttons[x].is_displayed():
-            driver.execute_script("arguments[0].click();", more_buttons[x])
-            time.sleep(4)
-    page_source = driver.page_source
-
-    soup = BeautifulSoup(page_source)
-    team_names = [tag.text for tag in soup.find_all(class_=["name"])]
-    bet_prices = [tag.text for tag in soup.find_all(class_=["bet-price","empty-bet"])]
-
-    # CURRENTLY ONLY INTERESTED IN MONEY LINES (3rd and 4th), spreads are 1 and 2, over/under 5 and 6
-    # spreads = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (1,2)]
-    # over_under = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (5,6)]
-
-    money_lines = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (3,4)]
-    teams_bets = [(i,t,b) for i,t,b in zip(range(len(team_names)),team_names, money_lines)]
-
-    team_1 = [tb[1] for tb in teams_bets if tb[0]%2 == 0]
-    team_2 = [tb[1] for tb in teams_bets if tb[0]%2 != 0]
-    team_1_bet = [tb[2] for tb in teams_bets if tb[0]%2 == 0]
-    team_2_bet = [tb[2] for tb in teams_bets if tb[0]%2 != 0]
-
-
-    bets_df = pd.DataFrame({'site': site,
-                            'sport':sport,
-                            'team_1': team_1,
-                            'team_2': team_2,
-                            'team_1_bet': team_1_bet,
-                            'team_2_bet': team_2_bet,})
-
-    bets = bets.append(bets_df, ignore_index=True)
-
-bets
-
-# BETONLINE AND BOVADA
-# SOMETIMES MISSING SOME
-# AND HOW TO IMPROVE
-from selenium import webdriver
-from bs4 import BeautifulSoup
-import pandas as pd
-import time
+import re
 
 bets = pd.DataFrame(columns=['site','sport','team_1','team_2','team_1_bet','team_2_bet'])
 site_tags = {'Bovada':{'Name': ['name'], 'Lines':['bet-price','empty-bet']},
@@ -80,41 +13,35 @@ url_info = {'Site': ['BetOnline',
                      'BetOnline',
                      'Bovada',
                      'Bovada',
-                     'Bovada'],
+                     'Bovada',
+                    ],
             'Sport': ['NFL',
                       'NHL/VHL',
                       'NBA',
                       'NFL',
-                      'NHL/VHL'],
+                      'NHL/VHL',
+                     ],
             'URL':['https://www.betonline.ag/sportsbook/football/nfl',
                    'https://www.betonline.ag/sportsbook/hockey/nhl',
                    'https://www.bovada.lv/sports/basketball/nba',
                    'https://www.bovada.lv/sports/football',
-                   'https://www.bovada.lv/sports/hockey'],}
+                   'https://www.bovada.lv/sports/hockey',
+                  ],}
 
 sites, sports, urls = url_info['Site'], url_info['Sport'], url_info['URL']
 
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+options.add_argument('--headless')
+driver = webdriver.Chrome("/Users/drake.hilliard/Desktop/chromedriver", options=options)
+
 for site, sport, url in zip(sites, sports, urls):
-    # should be able to set below up top
-    # seems to work better when redone each time though
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome("/Users/drake.hilliard/Desktop/chromedriver", options=options)
-    # should be able to set above up top
     driver.get(url)
-    more_buttons = driver.find_elements_by_class_name("moreLink")
-    for x in range(len(more_buttons)):
-        if more_buttons[x].is_displayed():
-            driver.execute_script("arguments[0].click();", more_buttons[x])
-            time.sleep(5)
+    time.sleep(3) # need, not only on clicks
     page_source = driver.page_source
 
     soup = BeautifulSoup(page_source)
-#     team_names = [tag.text for tag in soup.find_all(class_=["col_teamname bdevtt"])]
-#     money_lines = [tag.text for tag in soup.find_all(class_=["odds bdevtt moneylineodds displayOdds"])]
-
     team_names = [tag.text for tag in soup.find_all(class_=site_tags[site]['Name'])]
 
     if site == 'BetOnline':
@@ -123,14 +50,12 @@ for site, sport, url in zip(sites, sports, urls):
         bet_prices = [tag.text for tag in soup.find_all(class_=site_tags[site]['Lines'])]
         money_lines = [bp for i, bp in zip(range(len(bet_prices)),bet_prices) if (i+1) % 6 in (3,4)]
 
-
     teams_bets = [(i,t,b) for i,t,b in zip(range(len(team_names)),team_names, money_lines)]
 
     team_1 = [tb[1] for tb in teams_bets if tb[0]%2 == 0]
     team_2 = [tb[1] for tb in teams_bets if tb[0]%2 != 0]
     team_1_bet = [tb[2] for tb in teams_bets if tb[0]%2 == 0]
     team_2_bet = [tb[2] for tb in teams_bets if tb[0]%2 != 0]
-
 
     bets_df = pd.DataFrame({'site': site,
                             'sport':sport,
@@ -141,4 +66,37 @@ for site, sport, url in zip(sites, sports, urls):
 
     bets = bets.append(bets_df, ignore_index=True)
 
+for b in bets.columns:
+    bets[b] = bets[b].str.strip()
+
+for col in ['team_1_bet', 'team_2_bet']:
+    bets.loc[bets[col]=='EVEN',col] = '100'
+
+def is_int(someshit,r=True):
+    try:
+        int(someshit)
+    except:
+        r = False
+
+    return r
+
+def sign(x):
+    if is_int(x):
+        if int(x) > 0:
+            r = 1
+        elif int(x) < 0:
+            r = -1
+        else:
+            r = 0
+    else:
+        r = ''
+
+    return r
+
+def moneyline_factor(k):
+    (int(k) / 100) ** sign(int(k))
+
 bets
+bets_bovada = bets[bets['site']=='Bovada']
+bets_betonline = bets[bets['site']=='BetOnline']
+bets_joined = bets_bovada.merge(bets_betonline, how='inner', on=['sport','team_1','team_2'])
